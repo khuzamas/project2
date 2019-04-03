101b46a77d6d86f8fd4add1dae48d0bf160561d2
khuzamas <khuzamshu@gmail.com>
2019-04-03 16:20:25 +0300

    Updates

---
+++ precache-manifest.5c506971ab45cf07b8ddf6d12f24ea5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6f8e5937cde4dd88d6b51d1b023ed9",
+    "url": "/project2.git/static/media/waltographUI.ef6f8e59.ttf"
+  },
+  {
+    "revision": "a3b2a2a9a3d6078597b8",
+    "url": "/project2.git/static/js/runtime~main.f417762c.js"
+  },
+  {
+    "revision": "d61350df6774a9293b06",
+    "url": "/project2.git/static/js/main.6dcaead3.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/project2.git/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d61350df6774a9293b06",
+    "url": "/project2.git/static/css/main.1e8c5466.chunk.css"
+  },
+  {
+    "revision": "6469b87193eb5936a4f67a4899fc9650",
+    "url": "/project2.git/index.html"
+  }
+];
